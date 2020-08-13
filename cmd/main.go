67cc9974c6245462febf2a7b6c4a6c48291d0da2
uberballo <kasper.paavolainen@hotmail.com>
2020-08-13 15:24:03 +0300

    Add timeout function

--- cmd/main.go
+++ cmd/main.go
@@ -5,5 +5,3 @@ import "web-scraper/cmd/scraper"
 func main() {
 	scraper.Scrape("https://www.kauppalehti.fi/porssi/kurssit/XHEL")
 }
-
-///porssi/porssikurssit/osake/NOKIA/tilinpaatos
--- cmd/scraper/scraper.go
+++ cmd/scraper/scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
@@ -11,26 +12,54 @@ import (
 
 var testSuffix string = "porssi/porssikurssit/osake/NOKIA/tilinpaatos"
 var baseUrl string = "https://www.kauppalehti.fi"
+var url string = "https://www.kauppalehti.fi/porssi/kurssit/XHEL"
 
 func Scrape(url string) {
-	visible(url)
+	scrapeData(url, "")
 }
 
 func scrapeData(url, suffix string) {
-
+	collectElements(url, `.OMXH-list`, "el")
 }
 
-func visible(url string) {
+func visible(url, element string) error {
 	ctx, cancel := chromedp.NewContext(context.Background())
+	timeoutContext, cancel := context.WithTimeout(ctx, 1*time.Minute)
 	defer cancel()
+
+	err := chromedp.Run(timeoutContext,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(element),
+	)
+
+	if err == nil {
+		timeoutContext.Done()
+		return nil
+	}
+
+	return err
+}
+
+func collectElements(url, containerElement, element string) {
 
 	var res []string
 	var nodes []*cdp.Node
+	var err error
 
 	fmt.Println("Starting scraping")
-	err := chromedp.Run(ctx,
+
+	err = visible(url, containerElement)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("container element found")
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+	fmt.Println("new cont")
+
+	err = chromedp.Run(ctx,
 		chromedp.Navigate(url),
-		chromedp.WaitVisible(`.OMXH-list`),
 		chromedp.Nodes(`div[class="list-striped mb-1"] > div > a`, &nodes, chromedp.ByQueryAll),
 	)
 
